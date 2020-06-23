00aae7f237ec3f8a069b6ef082d968133cb78473
tarim1008 <kkfp@debian>
2020-06-23 15:58:54 +0200

    Voila la premiere branche

--- src/mon_projet/affiche.java
+++ src/mon_projet/affiche.java
@@ -5,6 +5,7 @@ public class affiche {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour la famille Git");
+		System.out.println("Creation de la premiere branche");
 
 	}
 
