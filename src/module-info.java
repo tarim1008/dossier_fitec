944ddbf85dbcaf484a98bcf73eee22390bb13a5b
tarim1008 <kkfp@debian>
2020-06-23 15:50:30 +0200

    Mon premier Commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module mon_projet {
+}
---
+++ src/mon_projet/affiche.java
@@ -0,0 +1,11 @@
+package mon_projet;
+
+public class affiche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour la famille Git");
+
+	}
+
+}
